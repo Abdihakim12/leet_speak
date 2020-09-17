d8f85ef95a4613413bb3372048f4446b1b37d9fa
Abdihakim Hassan <h_abdihakimi@yahoo.com>
2020-09-17 19:44:52 +0300

    update the project

--- lib/leet_speak.rb
+++ lib/leet_speak.rb
@@ -1,11 +1,12 @@
-def leetspeak(str)
-   return str.gsub(/[eoIs]/,
-     'e' => '3',
-     'o' => '0',
-     'I' => '1',
-     's' => 'z',
-)
 
-    str
+class  String
+  def leetspeak
+    self.gsub(/[eoI]/, 'e'=>'3','o'=>'0','I'=>'1')
+        .gsub(/(\S)(s)/, '\1z')
+         .gsub(/(\Ss)(ss)/, '\1zz')
+    
+    
+  end
+  
 end
-p leetspeak('sassafrass')
+p  "roses".leetspeak
--- spec/leet_speak_spec.rb
+++ spec/leet_speak_spec.rb
@@ -1,22 +1,23 @@
 require('rspec')
 require('leet_speak')
-describe('#leetspeak') do
+describe('String#leetspeak') do
     it('returns a string as is when no leetspeak rules apply') do
-        expect(leetspeak("Happy")).to(eq("Happy"))
+        expect("Happy".leetspeak).to(eq("Happy"))
     end 
     it('replaces every "e" in a string with a "3"') do
-        expect(leetspeak("elephant")).to(eq("3l3phant"))
+        expect("elephant".leetspeak).to(eq("3l3phant"))
       end
       it('replaces every "o" in a string with a "0"') do 
-        expect(leetspeak('boo boo')).to(eq('b00 b00'))
+        expect("boo boo".leetspeak).to(eq('b00 b00'))
       end
       it('replaces every "I" in a string with a "1" ') do 
-        expect(leetspeak('I like Ike')).to(eq('1 lik3 1k3'))
+        expect("I like Ike".leetspeak).to(eq('1 lik3 1k3'))
       end
       it('replaces every "s" in a string with a "z" ') do 
-        expect(leetspeak("roses")).to(eq("r0z3z"))
+        expect("roses".leetspeak).to(eq("r0z3z"))
       end
       it('does NOT replace the first letter when it is an s') do
         expect("sassafrass".leetspeak).to(eq("sazzafrazz"))
       end
+      
 end
